04c85938e3acd0c107132c2bb2de3f98669e3ba8
MAlonsoMA <MAlonsoMA@users.noreply.github.com>
2024-03-20 15:12:52 +0100

    Añadida aplicación redsocial para colocar enlaces a las redes sociales. Sólo aparecen aquellos que tienen contenido en el campo link

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'core',
     'portfolio.apps.PortfolioConfig',
     'about',
+    'redsocial.apps.RedsocialConfig',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'redsocial.processors.ctx_dict',
             ],
         },
     },
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ portafolio/redsocial/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RedsocialConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'redsocial'
+    verbose_name='Redes sociales'
